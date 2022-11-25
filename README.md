cbdb8a82c1413c4330460bce5a3989bb058c7bad
Erica <ericaanndelrio@gmail.com>
2022-11-24 22:05:19 -0500

    addedAllIfPrompts

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # hw3
+Started this project a little unsure of editing out each "div"
+working on Prompts (see screen shot notes Erica)
+click button to call prompts
--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(
+  );
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,4 +12,9 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); 
+prompt("Would you like 8, 10 or 12 characters?", "8, 10 or 12");
+if ("8", "10", "12" === prompt ("Would you like all caps, lowercase or both", "caps, lowercase, both"));
+if ("caps", "Caps", "CAPS", "lowercase", "Lowercase", "lower case", "both", "Both", "BOTH" === prompt ("Would you like to use special characters !@#$%^&*?", "yes or no"));
+if ("yes", "no", "Yes", "No", "YES", "NO" === true)
+

e7b61c2e6037b034c423d4b256a1f3d848f210aa
Erica <ericaanndelrio@gmail.com>
2022-11-27 21:06:56 -0500

    ableToSelectCharacterNumberAndYESnotNO

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+//1.  prompt user with password criteria
+//      a. 8-128
+//      b. lowercase, upper case, special characters
+var charNum = (prompt ("How many characters would you like to use?", "8 or 128"));
+  {
+    console.log("The client would like " + charNum + " characters")
+  }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword(
-  );
-  var passwordText = document.querySelector("#password");
+var specialChars = prompt ("Would you like to use UPPER CASE and special characters !@#$%^&*?", "yes or no");
+if (specialChars ===  "yes" ) {
+  console.log("The client would like special characters used")
+} 
+if (specialChars ===  "no" ) {
+  console.log("The client does not want special characters used")
+} 
 
-  passwordText.value = password;
+var generateBtn = document.querySelector("#generate"); // RED Generate Password button
+
+function generatePassword() {
+  var length = charNum,
+      yes = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklmnbvcxz!@#$%^&*",
+      no = "qwertyuiopasdfghjklzxcvbnm",
+      retVal = "";
+
+
+   for (var i = 0, n = yes.length; i < length; ++i) {
+       retVal += yes.charAt(Math.floor(Math.random() * n));
+       }
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
-prompt("Would you like 8, 10 or 12 characters?", "8, 10 or 12");
-if ("8", "10", "12" === prompt ("Would you like all caps, lowercase or both", "caps, lowercase, both"));
-if ("caps", "Caps", "CAPS", "lowercase", "Lowercase", "lower case", "both", "Both", "BOTH" === prompt ("Would you like to use special characters !@#$%^&*?", "yes or no"));
-if ("yes", "no", "Yes", "No", "YES", "NO" === true)
+//2.  validate input, ensure all above selected criteria is met
+//3.  generate password based on criteria
+//4.  display the password on the page
+// Assistance for this from Paul Keldsen
 
+function writePassword(
+
+)
+
+ {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); // uses the write password function

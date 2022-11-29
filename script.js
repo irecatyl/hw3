c83031a82c621c13c2686758277ae97ccd31c20e
Erica <ericaanndelrio@gmail.com>
2022-11-28 21:22:28 -0500

    setOptionYesORNo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //1.  prompt user with password criteria
 //      a. 8-128
 //      b. lowercase, upper case, special characters
-var charNum = (prompt ("How many characters would you like to use?", "8 or 128"));
+var charNum = (prompt ("How many characters would you like to use?", "8 to 128"));
   {
     console.log("The client would like " + charNum + " characters")
   }
@@ -22,22 +22,26 @@ function generatePassword() {
       yes = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklmnbvcxz!@#$%^&*",
       no = "qwertyuiopasdfghjklzxcvbnm",
       retVal = "";
+      
+  if (specialChars === "no") {
+      for (var x = 0, n = no.length; x < length; ++x) {
+        retVal += no.charAt(Math.floor(Math.random() * n));
+        }}
 
+  if (specialChars === "yes") {
+    for (var z = 0, y = yes.length; z < length; ++z) {
+      retVal += yes.charAt(Math.floor(Math.random() * y));
+      }}
 
-   for (var i = 0, n = yes.length; i < length; ++i) {
-       retVal += yes.charAt(Math.floor(Math.random() * n));
-       }
-
+  return retVal;
 }
 
 //2.  validate input, ensure all above selected criteria is met
 //3.  generate password based on criteria
 //4.  display the password on the page
-// Assistance for this from Paul Keldsen
+// Assistance for this from Paul Keldsen youtube
 
-function writePassword(
-
-)
+function writePassword()
 
  {
   var password = generatePassword();

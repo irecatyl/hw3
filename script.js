b873db7d6e34cc78a16e1fe09aa25d03dcc72abb
Erica <ericaanndelrio@gmail.com>
2022-12-04 14:28:48 -0500

    movedPromptsToGenerateBtn

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-// Assignment Code
-//1.  prompt user with password criteria
-//      a. 8-128
-//      b. lowercase, upper case, special characters
-var charNum = (prompt ("How many characters would you like to use?", "8 to 128"));
-  {
-    console.log("The client would like " + charNum + " characters")
-  }
-
-var specialChars = prompt ("Would you like to use UPPER CASE and special characters !@#$%^&*?", "yes or no");
-if (specialChars ===  "yes" ) {
-  console.log("The client would like special characters used")
-} 
-if (specialChars ===  "no" ) {
-  console.log("The client does not want special characters used")
-} 
 
 var generateBtn = document.querySelector("#generate"); // RED Generate Password button
+ 
 
 function generatePassword() {
-  var length = charNum,
-      yes = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklmnbvcxz!@#$%^&*",
-      no = "qwertyuiopasdfghjklzxcvbnm",
-      retVal = "";
+var charNum = prompt ("Choose a number of characters between 8 and 128", "8 to 128");
+  if (charNum < 8 || charNum> 128) {
+      prompt("Please enter a number between 8 and 128")} 
+  if (charNum >= 8 || charNum <= 128) {
+        console.log("The client would like " + charNum + " characters")
+      }
+    
+var specialChars = prompt ("Would you like to use UPPER CASE and special characters !@#$%^&*?", "yes or no");
+  if (specialChars ===  "yes" || "no" ) {
+      console.log("The client says "+ specialChars + " to using special characters.")
+    } else {
+      console.log("Please try again")}
+    
+var length = charNum;
+var yes = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklmnbvcxz!@#$%^&*";
+var no = "qwertyuiopasdfghjklzxcvbnm";
+var retVal = "";
       
   if (specialChars === "no") {
       for (var x = 0, n = no.length; x < length; ++x) {
@@ -32,23 +30,24 @@ function generatePassword() {
     for (var z = 0, y = yes.length; z < length; ++z) {
       retVal += yes.charAt(Math.floor(Math.random() * y));
       }}
+      return retVal;
 
-  return retVal;
 }
+
 
 //2.  validate input, ensure all above selected criteria is met
 //3.  generate password based on criteria
 //4.  display the password on the page
 // Assistance for this from Paul Keldsen youtube
 
-function writePassword()
-
+function writePassword ()
  {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); // uses the write password function
